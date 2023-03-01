1d393ee896af85d088b623893563cb671052f6f6
mominoffweb <mjamshidbek57@gmail.com>
2023-03-01 18:10:31 +0500

    Site-dark_mode

---
+++ js/app.js
@@ -0,0 +1,20 @@
+
+
+document.querySelector(".site-moon_img").addEventListener("click",()=> {
+    document.querySelector("body").classList.toggle("dark-mode")
+})
+
+document.querySelector(".site-moon_img").addEventListener("click",()=> {
+    document.querySelector("h1").classList.toggle("white")
+})
+
+
+document.querySelector(".site-moon_img").addEventListener("click",()=> {
+    document.querySelector("header-text").classList.toggle("dark-mode")
+})
+
+
+document.querySelector(".site-moon_img").addEventListener("click",()=> {
+    document.querySelector("text-main").classList.toggle("dark-mode")
+})
+
